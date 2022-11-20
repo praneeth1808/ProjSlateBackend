20de94df5b8f7dc391ed6f9f3e9a9e618c4f8c91
praneeth1808 <praneeth3331997@gmail.com>
2022-11-19 20:07:25 -0500

    save reset

--- JsonModel/jsonModel.py
+++ JsonModel/jsonModel.py
@@ -14,6 +14,7 @@ class JsonModel:
         model["LastModified"] = datetime.now(
         ).isoformat(timespec='microseconds')
         self.model = model
+        self.saveModel()
         return self.model
 
     def setLastModified(self):
